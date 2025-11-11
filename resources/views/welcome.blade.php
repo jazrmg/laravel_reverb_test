69c1e839d48379ccf13f45499d1fba5387d77cd9
jazremigio21 <jazpherremigio21@gmail.com>
2025-11-11 23:33:41 +0800

    working initial laravel reverb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1520,8 +1520,7 @@
 </head>
 
 <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-    <h1>Reverb Test Page</h1>
-    <p>Open your browser's developer console to see real-time messages.</p>
+    <div id="test"></div>
 </body>
 
 </html>
